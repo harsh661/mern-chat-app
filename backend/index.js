2d5bd7f8e2a1dcf792d12b9ffafacb0da3f88d14
harsh661 <plasmaharsh@gmail.com>
2023-09-21 23:09:05 +0530

    feat: initialize basic socket connection

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
-const router = require('./router')
-
+const express = require("express")
+const cors = require("cors")
+const socketio = require("socket.io")
+const http = require("http")
+const router = require("./router")
 const app = express()
 
+app.use(cors({ origin: "http://localhost:5173" }))
 
 const server = http.createServer(app)
 const io = socketio(server)
 
 app.use(router)
 
-io.on('connection', (socket) => {
-    console.log("A user connecter")
+io.on("connection", (socket) => {
+  console.log("A user connected")
 
-    socket.on('disconnect', () => {
-        console.log('User left')
-    })
+  socket.on("join", ({ name, roomId}, callback) => {
+    console.log({name, roomId})
+  })
+
+  socket.on("leave", () => {
+    console.log("User left")
+  })
 })
 
-server.listen(5000, () => {
-    console.log('Server started')
+server.listen(8000, () => {
+  console.log("Server started")
 })
--- client/components/Chat.tsx
+++ client/components/Chat.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import queryString from "query-string"
+import { useLocation } from "react-router"
+import {io, Socket} from "socket.io-client"
+
+let socket: Socket
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
+  const location = useLocation()
+
+  const [userName, setUserName] = useState("")
+  const [roomId, setRoomId] = useState("")
+
+  const ENDPOINT = "http://localhost:8000"
+
+  useEffect(() => {
+    const { name, roomId } = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+
+    setUserName(name as string)
+    setRoomId(roomId as string)
+
+    socket.emit('join', { name, roomId })
+
+    return () => {
+      socket.emit('leave')
+
+      socket.off()
+    }
+  }, [location.search, ENDPOINT])
+
+  return <div>Chat</div>
 }
 
 export default Chat
